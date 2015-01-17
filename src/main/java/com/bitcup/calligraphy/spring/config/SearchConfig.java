a24b88419a2ea46c28fbacbfbe9458397013242f
Bit Cup <omar2xba@gmail.com>
2015-01-16 21:34:42 -0500

    configs for deployment

--- src/main/java/com/bitcup/calligraphy/spring/config/SearchConfig.java
+++ src/main/java/com/bitcup/calligraphy/spring/config/SearchConfig.java
@@ -1,6 +1,8 @@
 package com.bitcup.calligraphy.spring.config;
 
 import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.node.Node;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,8 +28,10 @@ public class SearchConfig {
         // create an embedded Node that acts as a node within our cluster
         // this node does not hold data ==> client=true
         // q: can this node be created from the elasticsearch.yml config file in the classpath, without need to specify cluster name, etc.
+        /*
         Node node = nodeBuilder().loadConfigSettings(false).local(false).client(true).clusterName("callig").node();
         Client client = node.client();
+        */
         /* here's another way of connecting
         Settings settings = ImmutableSettings.settingsBuilder()
                 .put("cluster.name", "callig")
@@ -35,6 +39,15 @@ public class SearchConfig {
                 .build();
         Client client = new TransportClient(settings).addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
         */
+        Settings settings = ImmutableSettings.settingsBuilder()
+                .put("http.enabled", "false")
+                .put("transport.tcp.port", "9300-9400")
+                .put("discovery.zen.ping.multicast.enabled", "false")
+                .put("discovery.zen.ping.unicast.hosts", "localhost").build();
+
+        Node node = nodeBuilder().client(true).settings(settings).clusterName("callig").node();
+        Client client = node.client();
+
         logger.info("ES client node: {}", client.settings().toDelimitedString(",".charAt(0)));
         return new ElasticsearchTemplate(client);
     }
